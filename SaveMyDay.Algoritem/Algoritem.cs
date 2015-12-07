bc50136fb1a6eb75ffd8db6d1b11dd3d1058f14f
kilzid <kilzid@gmail.com>
2015-12-07 17:02:28 +0200

    add new class librarys

---
+++ SaveMyDay.Algoritem/Algoritem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaveMayDay.Common;
+using SaveMyDate.Entities;
+
+namespace SaveMyDay.Algoritem
+{
+    public class Algoritem
+    {
+        private IList<Constraint> _constraints;
+        private IList<Errand> _errands;
+        private IDictionary<CompanyType, IList<Appointment>> _appointmentDataBase;
+        private IList<Path> _results;
+
+        public Algoritem()
+        {
+            _constraints = new List<Constraint>();
+            _errands = new List<Errand>();
+        }
+
+        public bool Activate()
+        {
+            return false;
+        }
+
+        public Path GetResult(int priority)
+        {
+            return null;
+        }
+    }
+}
