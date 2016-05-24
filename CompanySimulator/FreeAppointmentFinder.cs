4d9fd9c95125e154a288040e425d9889767b003d
iahel ramos <iahelramos@gmail.com>
2016-05-24 16:15:04 +0300

    add stub for free appintment finder and api of companies

---
+++ CompanySimulator/FreeAppointmentFinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using SaveMyDate.Entities;
+
+namespace CompanySimulator
+{
+    public class FreeAppointmentFinder
+    {
+        public List<FreeAppointment> FindFreeAppointmentByDay(DateTime dayToScedual)
+        {
+            return new List<FreeAppointment>();
+        }
+    }
+}
---
+++ CompanySimulator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompanySimulator.WebApiApplication" Language="C#" %>
