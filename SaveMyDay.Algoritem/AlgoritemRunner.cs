1e3324e5658803d1e0c3e55b1de394bc372a51de
dror-heller <Drortreler@gmail.com>
2016-05-27 11:10:41 +0300

    algorithem vr 1

--- SaveMyDay.Algoritem/AlgoritemRunner.cs
+++ SaveMyDay.Algoritem/AlgoritemRunner.cs
@@ -10,22 +10,103 @@ namespace SaveMyDay.Algoritem
 {
     public class AlgoritemRunner
     {
-        public IList<Constraint> Constraints { get; private set; }
-        public IList<Errand> Errands { get; private set; }
-        public IDictionary<CompanyType, IList<Appointment>> AppointmentDataBase { get; private set; }
         public IList<Path> Results { get; private set; }
+        
+        private const int HOUR_MAX = 22;
+        private const int HOUR_MIN = 6;
+
+        private PathHandler[,] _routeMatrix;
 
         public AlgoritemRunner()
         {
-            AppointmentDataBase = new Dictionary<CompanyType, IList<Appointment>>();
-            Results = new List<Path>();
-            Constraints = new List<Constraint>();
-            Errands = new List<Errand>();
         }
 
-        public bool Activate(List<Company> companies)
+        public bool Activate(List<CompanyType> errands, List<Constraint> constraints, Dictionary<CompanyType,List<Appointment>> appointmentDataBase, Dictionary<Tuple<string,string>,int> deltaTimeMatrix)
         {
-            return false;
+            // the number of options in every row
+            var numberOfErrandCombinations = (int)Math.Pow(2, errands.Count);
+            _routeMatrix = new PathHandler[HOUR_MAX - HOUR_MIN, numberOfErrandCombinations];
+
+            var finalOptionList = new List<PathHandler>();
+
+            // for every hour in day, counting from the end
+            for (var currHour = HOUR_MAX-1; currHour >= HOUR_MIN; currHour--)
+            {
+                // set all empty path
+                _routeMatrix[currHour - HOUR_MIN,0] = new PathHandler(new Path { Constraints = constraints });
+
+                // for every errand combination in the row
+                for (var i = 1; i < numberOfErrandCombinations; i++)
+                {
+                    // check raised bits
+                    var bits = new bool[errands.Count];
+                    var tmp = i;
+                    for (var bit = 0; bit < errands.Count; bit++)
+                    {
+                        bits[bit] = tmp % 2 == 1;
+                        tmp /= 2;
+                    }
+
+                    var optionList = new List<PathHandler>();
+                    for (var bit = 0; bit < bits.Length; bit++)
+                    {
+                        if (bits[bit])
+                        {
+                            optionList.AddRange(FindAppointmentsBetweenTimes(currHour, currHour + 1, _routeMatrix[currHour-HOUR_MIN,i-(int)Math.Pow(2,bit)],
+                                appointmentDataBase[errands[bit]], deltaTimeMatrix));
+                            // 2 path combiner ?????? TODO:
+                        }
+                    }
+
+                    //pick best for the slot from option list
+                    PathHandler best = optionList[0];
+                    foreach (var option in optionList)
+                    {
+                        if (best.CalcWatedTimeInSeconds() > option.CalcWatedTimeInSeconds())
+                            best = option;
+                    }
+                    _routeMatrix[currHour - HOUR_MIN, i] = best;
+
+                    if(i == numberOfErrandCombinations)
+                        finalOptionList.AddRange(optionList);
+                }
+            }
+
+            ExtractResultFromPathHandlerList(FindTheBestPath(finalOptionList));
+            return true;
+        }
+
+        private List<PathHandler> FindAppointmentsBetweenTimes(int startHour, int endHour, 
+            PathHandler currPath, List<Appointment> appointments, Dictionary<Tuple<string, string>, int> deltaTimeMatrix)
+        {
+            var result = new List<PathHandler>();
+            foreach (var appointment in appointments.Where(
+                    a => a.Time.Hour > startHour && a.Time.Hour < endHour && currPath.IsAppointmentAddable(a, deltaTimeMatrix)).ToList())
+            {
+                var tmp = currPath.Clone();
+                tmp.AddAppointment(appointment);
+                result.Add(tmp);
+            }
+            return result;
+        }
+
+        private TimeSpan GetDeadTime(Appointment appointment, DateTime time, Dictionary<Tuple<string, string>, int> deltaTimeMatrix)
+        {
+            return appointment.Time - time; // TODO: add distance time
+        }
+
+        private List<PathHandler> FindTheBestPath(List<PathHandler> paths)
+        {
+            return paths.OrderByDescending(p => p.Path.Appointments.Count).ThenBy(p => p.CalcWatedTimeInSeconds()).ToList();
+        }
+
+        private void ExtractResultFromPathHandlerList(List<PathHandler> list)
+        {
+            Results.Clear();
+            foreach (var pathHandler in list)
+            {
+                Results.Add(pathHandler.Path);
+            }
         }
     }
 }
---
+++ SaveMyDay.Algoritem/PathHandler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaveMyDate.Entities;
+
+namespace SaveMyDay.Algoritem
+{
+    class PathHandler
+    {
+        public Path Path { get; private set; }
+
+        public PathHandler(Path path)
+        {
+            Path = path;
+        }
+
+        public PathHandler Clone()
+        {
+            var path = new Path();
+            path.Appointments.AddRange(Path.Appointments);
+            path.Constraints.AddRange(Path.Constraints);
+            path.type = Path.type;
+            return new PathHandler(path);
+        }
+        
+        public IList<CompanyType> GetErrandsCodes()
+        {
+            IList<CompanyType> erransdCods = new List<CompanyType>();
+            foreach (var appointment in Path.Appointments)
+            {
+                if(!erransdCods.Contains(appointment.Company.Type))
+                    erransdCods.Add(appointment.Company.Type);
+            }
+            return erransdCods;
+        }
+
+        public void AddAppointment(Appointment appointment)
+        {
+            Path.Appointments.Add(appointment);
+        }
+
+        public bool IsAppointmentAddable(Appointment appointment, Dictionary<Tuple<string, string>, int> deltaTimeMatrix)
+        {
+            //TODO: code
+            /* for all app/cons
+             * find the on in middle by times
+             * check by distance to see if fits V
+            */
+            return true;
+        }
+        
+        private TimeSpan GetDeadTime(/* app/cons 1, app/cons 2, matrix*/)
+        {
+            //return appointment.Time - time - matrix(id,id); // TODO: add distance time
+            return new TimeSpan();
+        }
+
+        public double CalcWatedTimeInSeconds()
+        {
+            IEnumerable<object> sorted = Path.Appointments.Concat<object>(Path.Constraints)
+               .OrderBy(n => n is Appointment ? ((Appointment)n).Time : ((Constraint)n).StartTime);
+
+            double diffInSeconds = 0;
+
+            DateTime before = DateTime.MinValue;
+            foreach (object either in sorted)
+            {
+                if (either is Appointment)
+                {
+                    if (before == DateTime.MinValue)
+                    {
+                        before = ((Appointment)either).Time.AddHours(1);
+                    }
+                    else
+                    {
+                        diffInSeconds += (((Appointment)either).Time - before).TotalMinutes;
+                        before = ((Appointment)either).Time.AddHours(1);
+                    }
+                }
+                else
+                {
+                    if (before == DateTime.MinValue)
+                    {
+                        before = ((Constraint)either).EndTime;
+                    }
+                    else
+                    {
+                        diffInSeconds += (((Constraint)either).StartTime - before).TotalMinutes;
+                        before = ((Constraint)either).EndTime;
+                    }
+                }
+            }
+
+            return diffInSeconds;
+        }
+    }
+}
