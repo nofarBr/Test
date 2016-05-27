303666af39744c243c4f041bb416fef711ef0fa3
dror-heller <Drortreler@gmail.com>
2016-05-27 19:09:34 +0300

    Comment for Ori's changes

--- SaveMyDay.WebApi/Controllers/FreeAppointmentTesterController.cs
+++ SaveMyDay.WebApi/Controllers/FreeAppointmentTesterController.cs
@@ -29,49 +29,49 @@ namespace SaveMyDay.WebApi.Controllers
         [HttpPost]
         public DbAppointmentCompany PostCompany()
         {
-            List<string> locs = new List<string>();
-            locs.Add("‏רוטשילד 78, ראשון לציון");
-            locs.Add("מורשת ישראל 15, ראשון לציון");
-            locs.Add("‏ההסתדרות 36, חולון");
-            locs.Add("חנקין 48, חולון");
-            locs.Add("הרצל 18, אשקלון");
-            locs.Add("ההסתדרות 40, אשקלון");
-            locs.Add("הרצל 20, גן יבנה");
-            locs.Add("המגינים 56, גן יבנה");
+            //List<string> locs = new List<string>();
+            //locs.Add("‏רוטשילד 78, ראשון לציון");
+            //locs.Add("מורשת ישראל 15, ראשון לציון");
+            //locs.Add("‏ההסתדרות 36, חולון");
+            //locs.Add("חנקין 48, חולון");
+            //locs.Add("הרצל 18, אשקלון");
+            //locs.Add("ההסתדרות 40, אשקלון");
+            //locs.Add("הרצל 20, גן יבנה");
+            //locs.Add("המגינים 56, גן יבנה");
 
-            foreach (string loc in locs)
-            {
-                Company company = new Company();
-                company.Id = ObjectId.GenerateNewId().ToString();
-                company.Location = loc;
-                company.SubType = "בנק הפועלים";
-                company.Type = CompanyType.PostOffice;
-                company.UrlForApi = "http://some.uri.com/";
-                _mongoCrudCompany.SaveOrUpdate(company);
+            //foreach (string loc in locs)
+            //{
+            //    Company company = new Company();
+            //    company.Id = ObjectId.GenerateNewId().ToString();
+            //    company.Location = loc;
+            //    company.SubType = "בנק הפועלים";
+            //    company.Type = CompanyType.PostOffice;
+            //    company.UrlForApi = "http://some.uri.com/";
+            //    _mongoCrudCompany.SaveOrUpdate(company);
 
-                DbAppointmentCompany fac = new DbAppointmentCompany();
-                fac.Company = company;
-                fac.Id = ObjectId.GenerateNewId().ToString();
+            //    DbAppointmentCompany fac = new DbAppointmentCompany();
+            //    fac.Company = company;
+            //    fac.Id = ObjectId.GenerateNewId().ToString();
 
-                DateTime testDate = DateTime.Now.Date;
-                testDate.AddHours(6);
+            //    DateTime testDate = DateTime.Now.Date;
+            //    testDate.AddHours(6);
 
-                DateTime endDate = testDate.AddMonths(2);
+            //    DateTime endDate = testDate.AddMonths(2);
 
-                List<string> times = new List<string>();
+            //    List<string> times = new List<string>();
 
-                while (testDate.ToString() != endDate.ToString())
-                {
-                    Console.WriteLine(testDate.ToString());
-                    fac.freeAppointments.Add(new DbAppointment(testDate, 30, "בנקאי", ObjectId.GenerateNewId().ToString()));
-                    times.Add(testDate.ToString("HH:mm:ss"));
-                    testDate = testDate.AddMinutes(30);
+            //    while (testDate.ToString() != endDate.ToString())
+            //    {
+            //        Console.WriteLine(testDate.ToString());
+            //        fac.freeAppointments.Add(new DbAppointment(testDate, 30, "בנקאי", ObjectId.GenerateNewId().ToString()));
+            //        times.Add(testDate.ToString("HH:mm:ss"));
+            //        testDate = testDate.AddMinutes(30);
 
-                    if (testDate.Hour == 21) { testDate.AddHours(9); }
-                }
+            //        if (testDate.Hour == 21) { testDate.AddHours(9); }
+            //    }
 
-                _mongoCrud.SaveOrUpdate(fac);
-            }
+            //    _mongoCrud.SaveOrUpdate(fac);
+            //}
             return null;
         }
     }
