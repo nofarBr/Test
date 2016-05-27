806e95c558c12b6a102147283993fba68e0c41d5
dror-heller <Drortreler@gmail.com>
2016-05-27 18:54:15 +0300

    Forgot file

--- SaveMyDay.WebApi/Controllers/FreeAppointmentTesterController.cs
+++ SaveMyDay.WebApi/Controllers/FreeAppointmentTesterController.cs
@@ -16,49 +16,63 @@ namespace SaveMyDay.WebApi.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class FreeAppointmentTesterController : ApiController
     {
-        private readonly MongoCrud<FreeAppointmentCompany> _mongoCrud;
-        private readonly MongoCrud<FreeAppointment> _mongoCrudFA;
+        private readonly MongoCrud<DbAppointmentCompany> _mongoCrud;
+        private readonly MongoCrud<DbAppointment> _mongoCrudFA;
         private readonly MongoCrud<Company> _mongoCrudCompany;
         public FreeAppointmentTesterController()
         {
-            _mongoCrud = new MongoCrud<FreeAppointmentCompany>();
+            _mongoCrud = new MongoCrud<DbAppointmentCompany>();
             _mongoCrudCompany = new MongoCrud<Company>();
-            _mongoCrudFA = new MongoCrud<FreeAppointment>();
+            _mongoCrudFA = new MongoCrud<DbAppointment>();
         }
 
         [HttpPost]
-        public FreeAppointmentCompany PostCompany(JObject jsonParam)
+        public DbAppointmentCompany PostCompany()
         {
-            Company company = _mongoCrudCompany.GetEntityById(jsonParam.GetValue("companyId").ToString());
-            FreeAppointmentCompany fac = new FreeAppointmentCompany();
-            fac.Company = company;
-            fac.Id = ObjectId.GenerateNewId().ToString();
+            List<string> locs = new List<string>();
+            locs.Add("‏רוטשילד 78, ראשון לציון");
+            locs.Add("מורשת ישראל 15, ראשון לציון");
+            locs.Add("‏ההסתדרות 36, חולון");
+            locs.Add("חנקין 48, חולון");
+            locs.Add("הרצל 18, אשקלון");
+            locs.Add("ההסתדרות 40, אשקלון");
+            locs.Add("הרצל 20, גן יבנה");
+            locs.Add("המגינים 56, גן יבנה");
 
-            DateTime testDate = DateTime.Now.Date;
-            testDate.AddHours(6);
-
-            DateTime endDate = testDate.AddYears(1);
-
-            List<string> times = new List<string>();
-
-            while (testDate.ToString() != endDate.ToString())
+            foreach (string loc in locs)
             {
-                Console.WriteLine(testDate.ToString());
-                fac.freeAppointments.Add(new FreeAppointment(testDate, 30, "בנקאי", ObjectId.GenerateNewId().ToString()));
-                times.Add(testDate.ToString("HH:mm:ss"));
-                testDate = testDate.AddMinutes(30);
-                
-                if (testDate.Hour == 21) { testDate.AddHours(9); }
-            }
+                Company company = new Company();
+                company.Id = ObjectId.GenerateNewId().ToString();
+                company.Location = loc;
+                company.SubType = "בנק הפועלים";
+                company.Type = CompanyType.PostOffice;
+                company.UrlForApi = "http://some.uri.com/";
+                _mongoCrudCompany.SaveOrUpdate(company);
 
-            //fac.freeAppointments;
+                DbAppointmentCompany fac = new DbAppointmentCompany();
+                fac.Company = company;
+                fac.Id = ObjectId.GenerateNewId().ToString();
 
-            foreach (FreeAppointment fa in fac.freeAppointments)
-            {
-                _mongoCrudFA.SaveOrUpdate(fa);
+                DateTime testDate = DateTime.Now.Date;
+                testDate.AddHours(6);
+
+                DateTime endDate = testDate.AddMonths(2);
+
+                List<string> times = new List<string>();
+
+                while (testDate.ToString() != endDate.ToString())
+                {
+                    Console.WriteLine(testDate.ToString());
+                    fac.freeAppointments.Add(new DbAppointment(testDate, 30, "בנקאי", ObjectId.GenerateNewId().ToString()));
+                    times.Add(testDate.ToString("HH:mm:ss"));
+                    testDate = testDate.AddMinutes(30);
+
+                    if (testDate.Hour == 21) { testDate.AddHours(9); }
+                }
+
+                _mongoCrud.SaveOrUpdate(fac);
             }
-            _mongoCrud.SaveOrUpdate(fac);
-            return fac;
+            return null;
         }
     }
 }
